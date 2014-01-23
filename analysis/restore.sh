033fe18abd1e7af79c99ff5115ba39d7a57ca422
ismartv-yh <yhyan@ubuntu.(none)>
2014-01-23 17:09:28 +0800

    .

--- analysis/restore.sh
+++ analysis/restore.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd `dirname $0`
-python dailytask.py 20131225 20140122 > /var/tmp/tasklog/dailytask/$(date +%Y%m%d%H).log 2>&1
+python dailytask.py 20131225 20140122 > /var/tmp/tasklog/dailytask/restore.log 2>&1
