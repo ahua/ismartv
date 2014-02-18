1c60c191be81e24396ee15beff5d3b3d197f6c5f
ismartv-yh <yhyan@ubuntu.(none)>
2014-02-18 10:35:12 +0800

    .

---
+++ analysis/hbase_api.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+import sys
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+from hbase import Hbase
+from hbase.ttypes import *
+
+class NotFoundTable(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class HbaseInterface:
+    def __init__(self, address, port, table):
+        self.tableName = table
+        self.transport = TTransport.TBufferedTransport(TSocket.TSocket(address, port))
+        self.protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
+        self.client = Hbase.Client(self.protocol)
+        self.transport.open()
+        tables = self.client.getTableNames()
+        if self.tableName not in tables:
+            raise NotFoundTable(self.tableName)
+            
+    def __del__(self):
+        self.transport.close()
+
+    def write(self, key, d):
+        mutations = []
+        for k,v in d.iteritems():
+            mutations.append(Mutation(column=k, value=v))
+        self.client.mutateRow(self.tableName, key, mutations, {})
+
+
+    def read(self, key, columns):
+        scannerId = self.client.scannerOpenWithPrefix(self.tableName, key, columns, None)
+        rowlist = self.client.scannerGet(scannerId)
+        self.client.scannerClose(scannerId)
+        if len(rowlist) >= 1:
+            return rowlist[0]
+        return None
+    
+    def read_all(self, prefix, columns):
+        scannerId = self.client.scannerOpenWithPrefix(self.tableName, prefix, columns, None)
+        rowlist = self.client.scannerGetList(scannerId, 100)
+        self.client.scannerClose(scannerId)
+        return rowlist
+
+
+def get_daily_data(date, device='ALL'):
+    client = HbaseInterface("localhost","9090","daily_result")    
+    colkeys = ["a:a", "a:b", "a:c", "a:d", "a:e", "a:f", "a:g", "a:h"]
+    rowlist = client.read_all(date, colkeys)
+    s = ('日期设备', '累计用户', '新增用户', '活跃用户', 'VOD用户',\
+             'VOD播放次数', 'VOD播放总时长', '应用激活用户', '智能激活用户')
+    for r in rowlist:
+        print r
+
+
+def get_weekly_data(date, device='ALL'):
+    client = HbaseInterface("localhost","9090","daily_result")    
+    colkeys = ["a:a", "a:b", "a:c", "a:d"]
+    rowlist = client.read_all(date, colkeys)
+    s = ('日期设备', '活跃用户', 'VOD用户', '应用激活用户', '智能激活用户')
+
+def get_monthly_data(date, device='ALL'):
+    client = HbaseInterface("localhost","9090","daily_result")    
+    colkeys = ["a:a", "a:b", "a:c", "a:d", "a:e", "a:f"]
+    rowlist = client.read_all(date, colkeys)
+    s = ('日期设备', '活跃用户', 'VOD用户','应用激活用户', \
+                  '智能激活用户', '首次缓冲3秒内(含)', '每次播放卡顿2次内')
+
+
+if __name__ == "__main__":
+    date = "20140102"
+    if len(sys.argv) == 2:
+        key = sys.argv[1]
+    get_daily_data(date)
+
