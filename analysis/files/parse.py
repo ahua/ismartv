4f04e22e793f2862f61aa26cf2c303662dbac597
ismartv-yh <yhyan@ubuntu.(none)>
2014-02-28 16:58:31 +0800

    .

--- analysis/files/parse.py
+++ analysis/files/parse.py
@@ -25,7 +25,7 @@ def process(filename):
                 continue
             code = r.get("code", "-")
             title = r.get("title", "-")
-            print "%s,%s" % (code, title)
+            print "%s,%s" % (code, title.replace(",", " "))
         except Exception as e:
             #print e
             continue
