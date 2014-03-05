ebf2535bef1a2b693be84e4c1e9f54493264c87b
ismartv-yh <yhyan@ubuntu.(none)>
2014-03-05 08:31:22 +0800

    .

--- analysis/dailytask.py
+++ analysis/dailytask.py
@@ -132,7 +132,7 @@ class DailyTask:
     def _f(self):
         sql = """select sum(duration), device
                  from daily_logs where parsets = '%s'
-                 and event = "video_exit"
+                 and event = "video_exit" and isplus = 1
                  group by device
               """ % self.day_str
         res = DailyTask.hiveinterface.execute(sql)
