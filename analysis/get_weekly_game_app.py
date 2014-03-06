759820d4e2bcb45b9acd150fb7a305c80afa080c
ismartv-yh <yhyan@ubuntu.(none)>
2014-03-06 15:35:31 +0800

    .

--- analysis/get_weekly_game_app.py
+++ analysis/get_weekly_game_app.py
@@ -7,38 +7,49 @@ import datetime
 def get_top50(date, dirname):
     filepath = os.path.join(dirname, "%s.txt" % date)
     d = {}
+    d_no_sharp = {}
     with open(filepath) as fp:
         for li in fp:
-            date, device, code, c = li.rstrip().split(",")
-            d[code] = int(c)
-    return d
+            date, device, code, c = li.rstrip().split(",")            
+            d[code] = int(c) + d.get(code, 0)
+            if device not in ["DS70A", "LX750A", "LX755A", "LX850A"]:
+                d_no_sharp = int(c) + d_no_sharp(code, 0)
+    return d, d_no_sharp
 
 def get_top10(date, dirname):
     day0 = datetime.datetime.strptime(sys.argv[1], "%Y%m%d")
     day1 = day0 - datetime.timedelta(days=7)
     
-    d0 = get_top50(date, dirname)
-    d1 = get_top50(day1.strftime("%Y%m%d"), dirname)
+    d0, d0_no_sharp = get_top50(date, dirname)
+    d1, d1_no_sharp = get_top50(day1.strftime("%Y%m%d"), dirname)
     
     d = {}
+    d_no_sharp = {}
+
     for k in d0:
         a = d0[k]
         b = d1.get(k, 0)
         d[k] = a*1.0/b if b != 0 else 0
-    return d
+
+    for k in d0_no_sharp:
+        a = d0_no_sharp[k]
+        b = d1_no_sharp.get(k, 0)
+        d_no_sharp[k] = a * 1.0 / b if b != 0 else 0
+
+    return d, d_no_sharp
 
 def get_code_name(code2):
     d = {}
     fp0 = "./files/0226_name_code.txt"
     with open(fp0) as fp:
         for li in fp:
-            name, code = li.rstrip().split(",")
+            name, code = li.rstrip().split(",")[0:2]
             d[code] = name
     
     fp1 = "./files/code_name.txt"
     with open(fp1) as fp:
         for li in fp:
-            code, name = li.rstrip().split(",")
+            code, name = li.rstrip().split(",")[0:2]
             d[code] = name
     return d.get(code2, "-")
 
@@ -60,7 +71,7 @@ if __name__ == "__main__":
     appdir = "/home/deploy/src/ismartv/analysis/result/app"
     gameappdir = "/home/deploy/src/ismartv/analysis/result/gameapp"
 
-    d = get_top50(date, appdir)
+    d, d_no_sharp = get_top50(date, appdir)
     print "APP应用活跃用户数top50应用排名；"
     print "id,code,tiltle,count"
     display_top(d)
@@ -69,20 +80,20 @@ if __name__ == "__main__":
 
     print "Game应用活跃用户数top50应用排名；"
     print "id,code,tiltle,count"
-    d = get_top50(date, gameappdir)
+    d, d_no_sharp = get_top50(date, gameappdir)
     display_top(d)
 
     display_newline()
 
     print "本周活跃用户数增长最快的应用top10；"
     print "id,code,tiltle,count"
-    d = get_top10(date, appdir)
+    d, d_no_sharp = get_top10(date, appdir)
     display_top(d, 10)
     
     display_newline()
 
     print "本周活跃用户数增长最快的游戏top10；"
     print "id,code,tiltle,count"
-    d = get_top10(date, gameappdir)
+    d, d_no_sharp = get_top10(date, gameappdir)
     display_top(d, 10)
 
