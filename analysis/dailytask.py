273a382f7a511282b7592da984546435890fd398
yhyan <yhyan@geek.(none)>
2014-03-16 08:35:20 +0800

    .

--- analysis/dailytask.py
+++ analysis/dailytask.py
@@ -33,7 +33,7 @@ class DailyTask:
         return DailyTask.sntable.read(key, ["a:device", "a:day"])
 
     def save_to_hbase(self, sn, device, day_str):
-        key = "sn_%s" % sn
+        key = "sn_%s_%s" % (day_str, sn)
         d = {"a:device": device, "a:day": day_str}
         DailyTask.sntable.write(key, d)
 
