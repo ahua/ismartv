6ce3e84f96677725931dd72845b51ba5d97f61eb
yhyan <yhyan@geek.(none)>
2014-03-22 17:32:31 +0800

    .

--- analysis/monthlytask.sh
+++ analysis/monthlytask.sh
@@ -2,4 +2,4 @@
 
 cd `dirname $0`
 python monthlytask.py > /var/tmp/tasklog/monthlytask/$(date +%Y%m%d%H).log 2>&1
-
+python monthlychannel.py > /var/tmp/tasklog/monthlychannel/$(date +%Y%m%d%H).log 2>&1
--- analysis/weeklytask.sh
+++ analysis/weeklytask.sh
@@ -2,4 +2,5 @@
 
 cd `dirname $0`
 python weeklytask.py > /var/tmp/tasklog/weeklytask/$(date +%Y%m%d%H).log 2>&1
+python weeklychannel.py > /var/tmp/tasklog/weeklychannel/$(date +%Y%m%d%H).log 2>&1
 
