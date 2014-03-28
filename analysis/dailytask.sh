8d56c7f82daa371e84c276c1763db0e7c45a62f1
ismartv-yh <yhyan@ubuntu.(none)>
2014-03-28 17:16:17 +0800

    .

--- analysis/dailytask.sh
+++ analysis/dailytask.sh
@@ -4,4 +4,5 @@ cd `dirname $0`
 python dailytask.py > /var/tmp/tasklog/dailytask/$(date +%Y%m%d%H).log 2>&1
 python get_sn_pos.py > /var/tmp/tasklog/get_sn_pos/$(date +%Y%m%d%H).log 2>&1
 python dailychannel.py > /var/tmp/tasklog/dailychannel/$(date +%Y%m%d%H).log 2>&1
+python dailytop.py > /var/tmp/tasklog/dailytop/$(date +%Y%m%d%H).log 2>&1
 
